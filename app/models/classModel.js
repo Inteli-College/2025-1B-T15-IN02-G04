7bbc555ff3ae12d363328b8e7c5cb88f30ef279b
Rafael Santana Rodrigues <138138154+RafaelSR44@users.noreply.github.com>
2025-05-29 20:38:00 -0300

    refactor: pequenas correções na class

--- app/models/classModel.js
+++ app/models/classModel.js
@@ -2,34 +2,69 @@ const db = require('../config/db');
 
 class ClassModel {
   static async getAllClasses() {
-    const result = await db.query('SELECT * FROM class');
-    return result.rows;
+    try {
+      const result = await db.query('SELECT * FROM class');
+      return result.rows;
+    } catch (error) {
+      console.error("Erro ao buscar todas as classes:", error);
+      throw error;
+    }
   }
 
   static async getClassById(id) {
-    const result = await db.query('SELECT * FROM class WHERE id = $1', [id]);
-    return result.rows[0];
+    try {
+      const result = await db.query('SELECT * FROM class WHERE id = $1', [id]);
+      return result.rows[0];
+    } catch (error) {
+      console.error("Erro ao buscar classe por ID:", error);
+      throw error;
+    }
+  }
+
+  static async getClassesByModuleId(moduleId) {
+    try {
+      const result = await db.query('SELECT * FROM class WHERE id_module = $1', [moduleId]);
+      return result.rows;
+    } catch (error) {
+      console.error("Erro ao buscar classes por módulo:", error);
+      throw error;
+    }
   }
 
   static async createClass(data) {
-    const result = await db.query(
-      'INSERT INTO class (name, description, id_module) VALUES ($1, $2, $3) RETURNING *',
-      [data.name, data.description, data.id_module]
-    );
-    return result.rows[0];
+    try {
+      const result = await db.query(
+        'INSERT INTO class (name, description, id_module) VALUES ($1, $2, $3) RETURNING *',
+        [data.name, data.description, data.id_module]
+      );
+      return result.rows[0];
+    } catch (error) {
+      console.error("Erro ao criar classe:", error);
+      throw error;
+    }
   }
 
-  static async updateClass(id, data) {
-    const result = await db.query(
-      'UPDATE class SET name = $1, description = $2 id_module = $3 WHERE id = $4 RETURNING *',
-      [data.name, data.description, data.id_module, id]
-    );
-    return result.rows[0];
+  static async updateClass(id, name, description, id_module) {
+    try {
+      const result = await db.query(
+        'UPDATE class SET name = $1, description = $2, id_module = $3 WHERE id = $4 RETURNING *',
+        [name, description, id_module, id]
+      );
+      return result.rows[0];
+    } catch (error) {
+      console.error("Erro ao atualizar classe:", error);
+      throw error;
+    }
   }
 
   static async deleteClass(id) {
-    const result = await db.query('DELETE FROM class WHERE id = $1 RETURNING *', [id]);
-    return result.rowCount > 0;
+    try {
+      const result = await db.query('DELETE FROM class WHERE id = $1', [id]);
+      return result.rowCount > 0;
+    } catch (error) {
+      console.error("Erro ao deletar classe:", error);
+      throw error;
+    }
   }
 }
 
