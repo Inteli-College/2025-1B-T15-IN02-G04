e4e4a5df881f624d8d04795160f6db91c31baf32
Leonardo Cesar Corbi <leonardocorbi@Leonardos-MacBook-Air.local>
2025-06-12 15:17:07 -0300

    fix: arruma path para userModel no userController

--- app/controllers/userController.js
+++ app/controllers/userController.js
@@ -1,6 +1,6 @@
 // controllers/userController.js
 
-const userController = require('../models/userModels');
+const userController = require("../models/userModel");
 
 const UserController = {
   async getAllUsers(req, res) {
@@ -17,40 +17,47 @@ const UserController = {
       const { id } = req.params;
       const user = await UserModels.getUserById(id);
       if (!user) {
-        return res.status(404).json({ error: 'Usuário não encontrado' });
+        return res.status(404).json({ error: "Usuário não encontrado" });
       }
       return res.status(200).json(user);
     } catch (error) {
       console.error(error);
-      return res.status(500).json({ error: 'Erro ao obter usuário.' });
+      return res.status(500).json({ error: "Erro ao obter usuário." });
     }
   },
 
-  async createUser (req, res) {
+  async createUser(req, res) {
     try {
       const newUser = await UserModels.createUser(req.body);
       return res.status(201).json(newUser);
-      } catch (err) {
-        console.error('Erro ao criar usuário:', err); 
-        res.status(500).json({ error: 'Erro ao criar usuário' });
-      }
+    } catch (err) {
+      console.error("Erro ao criar usuário:", err);
+      res.status(500).json({ error: "Erro ao criar usuário" });
+    }
   },
-  
 
-    async updateUser(req, res) {
+  async updateUser(req, res) {
     try {
-      const { first_name, last_name, email, hash_password, username } = req.body;
+      const { first_name, last_name, email, hash_password, username } =
+        req.body;
       console.log("REQ.BODY:", req.body);
       console.log("FIRST_NAME:", first_name);
       console.log("LAST_NAME:", last_name);
       console.log("EMAIL:", email);
       console.log("HASH_PASSWORD:", hash_password);
       console.log("USERNAME:", username);
-      const updatedUser = await UserModels.updateUser(req.params.id,first_name, last_name, email, hash_password, username);
+      const updatedUser = await UserModels.updateUser(
+        req.params.id,
+        first_name,
+        last_name,
+        email,
+        hash_password,
+        username
+      );
       if (updatedUser) {
-      res.status(200).json(updatedUser);
+        res.status(200).json(updatedUser);
       } else {
-      res.status(404).json({ error: 'Usuário não encontrado' });
+        res.status(404).json({ error: "Usuário não encontrado" });
       }
     } catch (error) {
       res.status(500).json({ error: error.message });
@@ -63,7 +70,7 @@ const UserController = {
       if (deletedUser) {
         res.status(200).json(deletedUser);
       } else {
-        res.status(404).json({ error: 'Usuário não encontrado' });
+        res.status(404).json({ error: "Usuário não encontrado" });
       }
     } catch (error) {
       res.status(500).json({ error: error.message });
