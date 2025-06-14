e66d4038cdae082ad687aaf024281e030a957940
Mariana Pereira <mariana.pereira@sou.inteli.edu.br>
2025-06-13 23:55:23 -0300

    Add files via upload

---
+++ app/routes/classRoutes.js
@@ -0,0 +1,11 @@
+const express = require('express');
+const router = express.Router();
+const classController = require('../controllers/classController');
+
+router.get('/classes', classController.getAllClasses);
+router.get('/classes/:id', classController.getClassById);
+router.post('/classes', classController.createClass);
+router.put('/classes/:id', classController.updateClass);
+router.delete('/classes/:id', classController.deleteClass);
+
+module.exports = router;
